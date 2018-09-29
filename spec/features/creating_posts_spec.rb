2b76c9c0b0be1e2f91d7516d91a65a8464ea5f6b
josh nickson <jnickson1506@gmail.com>
2018-09-29 17:18:11 +0100

    generate rails posts controller and add routes plus actions

--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -3,11 +3,14 @@ require 'rails_helper'
 RSpec.feature "Create posts", type: :feature do
   scenario "user creates a post" do
     visit '/'
-    click_link 'New Post'
-    attach_file('Image', "test.jpg")
+    puts page.body
+    click_on 'New Post'
+    attach_file('Image', File.join(Rails.root, '/spec/tesla-cat.jpg'))
+
+
     fill_in 'Caption', with: 'test #test' 
     click_button 'Create Post'
     expect(page).to have_content('#test')
-    expect(page).to have_css("img[src*='test.jpg']")
+    expect(page).to have_css("img[src*='tesla-cat.jpg']")
   end
 end
