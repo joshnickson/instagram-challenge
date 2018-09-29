5f9ac11df8cfbd50361b62b396d7aacaf716f38b
josh nickson <jnickson1506@gmail.com>
2018-09-29 20:12:06 +0100

    feat: add validation for image when posting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,13 @@ class PostsController < ApplicationController
   
   def create
     @post = Post.create(post_params)
-    redirect_to @post
+    if @post.save
+      flash[:success] = "Your post has been created"
+      redirect_to @post 
+    else
+      flash[:alert] = "You must add an image!"
+      render :new
+    end
   end
 
   def post_params
--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -5,12 +5,16 @@ RSpec.feature "Create posts", type: :feature do
     visit '/'
     click_on 'New Post'
     attach_file('Image', File.join(Rails.root, '/spec/tesla-cat.jpg'))
-
-
     fill_in 'Caption', with: 'test #test' 
     click_button 'Create Post'
-    puts page.body
     expect(page).to have_content('#test')
     expect(page).to have_css("img[src*='tesla-cat.jpg']")
+  end
+  scenario 'image is required to make a post' do
+    visit '/'
+    click_link 'New Post'
+    fill_in 'Caption', with: 'test #test' 
+    click_button 'Create Post'
+    expect(page).to have_content('You must add an image!')
   end
 end
