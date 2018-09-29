3f17a327aec8dd955329198dee7e31a2ab59fb57
josh nickson <jnickson1506@gmail.com>
2018-09-29 21:16:24 +0100

    feat: user can view single post

--- spec/features/displaying_posts_spec.rb
+++ spec/features/displaying_posts_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.feature "Create posts", type: :feature do
-  scenario "user creates a post" do
+RSpec.feature "Diplaying posts", type: :feature do
+  scenario "index shows mulitple posts" do
     post_one = create(:post, caption: "first post")
     post_two = create(:post, caption: "second post")
 
@@ -11,4 +11,15 @@ RSpec.feature "Create posts", type: :feature do
     puts page.body
     expect(page).to have_css("img[src*='tesla-cat.jpg']")
   end 
+  
+end
+
+RSpec.feature "Single post view", type: :feature do
+    scenario 'user can view a single post' do
+    post = create(:post)
+
+    visit '/'
+    find(:xpath, "//a[contains(@href,'posts/1')]").click
+    expect(page.current_path).to eq(post_path(post))
+  end
 end
